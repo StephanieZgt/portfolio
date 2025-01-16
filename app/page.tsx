cc19cf7f1a657f522acff1ce00e6a0af6d3602aa
Ludovic ZWINGELSTEIN <ludovic.zwingelstein@adevinta.com>
2025-01-16 01:41:26 +0100

    Remove skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/HeroSection";
 import "./globals.css";
-import SkillsSection from "@/components/SkillsSection";
 import Footer from "@/components/Footer";
 import ProjectSection from "@/components/ProjectSection";
 export default function Home() {
@@ -9,8 +8,6 @@ export default function Home() {
       <div className="px-4 md:px-8 xl:px-32 relative">
         <HeroSection />
         <hr className="w-full text-white" />
-        <SkillsSection />
-        <hr className="md:hidden w-full text-white" />
         <ProjectSection />
       </div>
       <Footer />
